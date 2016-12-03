ffc776bc94a9c4b471ed32f1b6fdf49df624aa99
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-12-03 18:34:03 -0500

    added react

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const App = React.createClass({
+    render() {
+        return (
+            <div>
+                <h2>My site</h2>
+            </div>
+        )
+    }
+})
+
+module.exports = App
